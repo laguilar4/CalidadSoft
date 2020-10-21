dba8fbab260a98475a7e02f92f41ab0ba831b6fb
Luis Aguilar <laguilar4@cuc.edu.co>
2020-10-20 21:10:23 -0500

    Falta editar,borrar y otras funciones

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('index');
+});
+Route::get('/agregardoctores', 'DoctoresController@agregardoc')->name('agregardoctores');
+Route::get('/inicio', 'DoctoresController@index')->name('inicio');
+Route::get('/listadodoctores', 'DoctoresController@listasdoc')->name('listadodoctores');
+Route::post('/store', 'DoctoresController@store')->name('store');
+Route::get('/agregarpacientes', 'PacientesController@agregarpac')->name('agregarpacientes');
+Route::get('/listadopacientes', 'PacientesController@listaspac')->name('listadopacientes');
+Route::post('/store2', 'PacientesController@store')->name('store2');
+Route::get('/agregarmedicamentos', 'MedicamentosController@agregarmedic')->name('agregarmedicamentos');
+Route::get('/listadomedicamentos', 'MedicamentosController@listasmedic')->name('listadomedicamentos');
+Route::post('/store3', 'MedicamentosController@store')->name('store3');
